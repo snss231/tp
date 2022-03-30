cecc249f7552fc47b51524a65b7ce2680d1cc0c3
AdrianOngJJ <adrianong360@gmail.com>
2022-03-31 03:26:58 +0800

    Cleanup UG and change find command

--- src/main/java/seedu/address/model/task/TaskNameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/task/TaskNameContainsKeywordsPredicate.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
+import seedu.address.model.tag.Tag;
 
 /**
  * Tests that a {@code Task}'s {@code Name} matches any of the keywords given.
@@ -21,6 +22,17 @@ public class TaskNameContainsKeywordsPredicate implements Predicate<Task> {
         for (String str: keywords) {
             if (StringUtil.containsWordIgnoreCase(task.getName(), str.trim())) {
                 isEqual = true;
+                break;
+            }
+            Tag[] tags = new Tag[task.getTags().size()];
+            task.getTags().toArray(tags);
+            for (int i = 0; i < tags.length; i++ ) {
+                if (StringUtil.containsWordIgnoreCase(tags[i].toString(), str.trim())) {
+                    isEqual = true;
+                    break;
+                }
+            }
+            if (isEqual) {
                 break;
             }
         }
