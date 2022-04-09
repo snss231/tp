6f10e879c90bc75903e991c12f0d9f79bcb78b0b
junrong98 <e0544375@u.nus.edu>
2022-04-09 11:50:28 +0800

    Add test cases for link

--- src/main/java/seedu/address/model/task/Link.java
+++ src/main/java/seedu/address/model/task/Link.java
@@ -2,6 +2,10 @@ package seedu.address.model.task;
 
 import static java.util.Objects.requireNonNull;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class Link {
     public static final String MESSAGE_CONSTRAINTS = "The link provided should follows the proper URL format.";
     private String link;
@@ -13,11 +17,27 @@ public class Link {
      */
     public Link(String link) {
         requireNonNull(link);
+
         this.link = link;
     }
 
     public Link() {
         this.link = null;
+    }
+
+    /**
+     * Check if link is valid
+     * @param link Link of users
+     * @throws IllegalArgumentException If link is not a valid URL
+     */
+    public static boolean isValidLink(String link) {
+        try {
+            new URL(link).toURI();
+        } catch (URISyntaxException | MalformedURLException e) {
+            // Unconventional, but currently only way to check whether a link is valid.
+            return false;
+        }
+        return true;
     }
 
     @Override
