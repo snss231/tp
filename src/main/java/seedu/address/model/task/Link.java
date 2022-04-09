76716721ff27bfa52f28c65da328cda9b1114a55
junrong98 <77258042+junrong98@users.noreply.github.com>
2022-04-10 01:08:50 +0800

    Update src/main/java/seedu/address/model/task/Link.java
    Co-authored-by: junjunjieOng <e0544320@u.nus.edu>

--- src/main/java/seedu/address/model/task/Link.java
+++ src/main/java/seedu/address/model/task/Link.java
@@ -27,7 +27,7 @@ public class Link {
 
     /**
      * Checks if link is valid.
-     * @param link Link of users
+     * @param link Link of users.
      * @throws IllegalArgumentException If link is not a valid URL.
      */
     public static boolean isValidLink(String link) {
